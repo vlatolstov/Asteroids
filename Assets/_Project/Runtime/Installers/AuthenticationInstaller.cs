45c1a4eb6bd1367b96e521b4d6c59567b42992bd
Vladimir <vlatolstov@gmail.com>
2026-03-11 22:48:36 +0400

    Packages added

---
+++ Assets/_Project/Runtime/Installers/AuthenticationInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace _Project.Runtime.Installers
+{
+    public class AuthenticationInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
